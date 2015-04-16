ec75c886ee02ab3dca674ed1def04c994945cac9
hcumbicusr <hcumbicusr@gmail.com>
2015-04-16 12:46:29 -0500

    uso jqmobile

--- js/index.js
+++ js/index.js
@@ -17,37 +17,40 @@
  * under the License.
  */
 $(document).ready(function(){
-    
+
     initialize();
-        // Application Constructor
-        function initialize() {
-            bindEvents();
-        }
-        // Bind Event Listeners
-        //
-        // Bind any events that are required on startup. Common events are:
-        // 'load', 'deviceready', 'offline', and 'online'.
-        function bindEvents() {
-            document.addEventListener('deviceready', onDeviceReady, false);
-        }
-        // deviceready Event Handler
-        //
-        // The scope of 'this' is the event. In order to call the 'receivedEvent'
-        // function, we must explicity call 'app.receivedEvent(...);'
-        function onDeviceReady() {
-            receivedEvent('deviceready');
-        }
-        // Update DOM on a Received Event
-        function receivedEvent(id) {
-            var parentElement = document.getElementById(id);
-            var listeningElement = parentElement.querySelector('.listening');
-            var receivedElement = parentElement.querySelector('.received');
-
-            listeningElement.setAttribute('style', 'display:none;');
-            receivedElement.setAttribute('style', 'display:block;');
-
-            console.log('Received Event: ' + id);               
-
-        }
+    // Application Constructor
+    function initialize() {
+        bindEvents();
+    }
+    // Bind Event Listeners
+    //
+    // Bind any events that are required on startup. Common events are:
+    // 'load', 'deviceready', 'offline', and 'online'.
+    function bindEvents() {
+        document.addEventListener('deviceready', onDeviceReady, false);
+    }
+    // deviceready Event Handler
+    //
+    // The scope of 'this' is the event. In order to call the 'receivedEvent'
+    // function, we must explicity call 'app.receivedEvent(...);'
+    function onDeviceReady() {
+        console.log("Page: 1");
+        receivedEvent('sistrado');
+        console.log("Page: 2");
+        //--------- Aqui la carga se ha completado correctamente --------------
+        
+    }
+    // Update DOM on a Received Event
+    function receivedEvent(id) {
+        var parentElement = document.getElementById(id);
+        var listeningElement = parentElement.querySelector('.listening');
+        var receivedElement = parentElement.querySelector('.received');
+        // solo se muestra si no hay conexion
+        listeningElement.setAttribute('style', 'display:none;');
+        //receivedElement.setAttribute('style', 'display:block;');                
+        
+        console.log('Received Event: ' + id);
+    }
     
 });
